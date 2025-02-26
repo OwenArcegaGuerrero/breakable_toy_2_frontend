17dc2ce2fd2709fd2e0df7b7f60448ff6c90537f
= <=>
2025-02-26 15:03:07 -0600

    added-login

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-function App() {
-  return <></>;
-}
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginComponent from "./components/Login";
+import Dashboard from "./components/Dashboard";
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginComponent />} />
+        <Route path="/" element={<Dashboard />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store.ts";
 import App from "./App.tsx";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
